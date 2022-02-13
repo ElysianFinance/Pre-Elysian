1f2a95b5fdfdf5c789cb645a4bdcac60901568d3
kingluxor <luxor@elysian.finance>
2022-02-13 17:41:37 -0400

    use npm package

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const elysianDeployment = require('@elysianfinance/elysian-contracts/deployed/ganache/deployment.json');
+
+const {
+    green,
+    red,
+    gray,
+    yellow
+} = require("chalk");
 
 const PreElysianToken = artifacts.require("PreElysianToken");
 const ExercisePLYS = artifacts.require("ExercisePLYS");
 
-const LYS = "0x9EBB9D9d463fD0D5CFD6C68f7a517C8C781B4099";
+const LYS = elysianDeployment.targets["ProxyElysian"].address;
+const Treasury = elysianDeployment.targets["MyTreasury"].address;
 const DAI = "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3";
-const Treasury = "0xEC9b75D89C48EbFc274aa93Ce0F7C59a12Aa5eB2";
 
 const deploymentFile = require("../deployment.json");
 
@@ -22,9 +30,7 @@ const saveBuild = (deployment) => {
             }
         }
     );
- 
 }
-
 
 async function doDeploy(deployer, network, accounts) {
 
@@ -32,7 +38,7 @@ async function doDeploy(deployer, network, accounts) {
         PreElysianToken, accounts[0]
     );
 
-    console.log(`PreElysianToken deployed to: ${PreElysianTokenContract.address} on network: ${network}`);
+    console.log(`PreElysianToken deployed to: ${gray(PreElysianTokenContract.address)} on network: ${yellow(network)}`);
 
     let ExercisePLYSContract = await deployer.deploy(
         ExercisePLYS,
@@ -42,7 +48,7 @@ async function doDeploy(deployer, network, accounts) {
         Treasury,
     );
 
-    console.log(`ExercisePLYSContract deployed to: ${ExercisePLYSContract.address} on network: ${network}`);
+    console.log(`ExercisePLYSContract deployed to: ${gray(ExercisePLYSContract.address)} on network: ${yellow(network)}`);
 
     const deployment = {
         PreElysianToken: PreElysianTokenContract.address,
