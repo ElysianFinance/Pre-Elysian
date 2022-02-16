e2d73ec8e07cc3989d4dc92653475ec036be22b0
kingluxor <luxor@elysian.finance>
2022-02-16 18:46:04 -0400

    deploy Vault

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,8 +11,9 @@ const {
 
 const PreElysianToken = artifacts.require("PreElysianToken");
 const ExercisePLYS = artifacts.require("ExercisePLYS");
+const Vault = artifacts.require("Vault");
 
-const LYS = elysianDeployment.targets["ProxyElysian"].address;
+const LYS = elysianDeployment.targets["Elysian"].address;
 const Treasury = elysianDeployment.targets["MyTreasury"].address;
 const DAI = "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3";
 
@@ -40,19 +41,26 @@ async function doDeploy(deployer, network, accounts) {
 
     console.log(`PreElysianToken deployed to: ${gray(PreElysianTokenContract.address)} on network: ${yellow(network)}`);
 
+    let VaultContract = await deployer.deploy(
+        Vault, accounts[0]
+    );
+
+    console.log(`Vault deployed to: ${gray(VaultContract.address)} on network: ${yellow(network)}`);
+
     let ExercisePLYSContract = await deployer.deploy(
         ExercisePLYS,
         PreElysianTokenContract.address, 
         LYS,
         DAI,
-        Treasury,
+        VaultContract.address,
     );
 
     console.log(`ExercisePLYSContract deployed to: ${gray(ExercisePLYSContract.address)} on network: ${yellow(network)}`);
 
     const deployment = {
         PreElysianToken: PreElysianTokenContract.address,
-        ExercisePLYS: ExercisePLYSContract.address
+        ExercisePLYS: ExercisePLYSContract.address,
+        Vault: VaultContract.address
     }
 
     deploymentFile.networks[network] = deployment;
