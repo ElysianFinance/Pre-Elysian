4a33138126a7a295e1db84ae786f7c2eed2db711
kingluxor <luxor@elysian.finance>
2022-02-18 17:58:38 -0400

    add setTreasury, fix func. name, add back requires, use IElysian interface

--- contracts/ExercisePLYS.sol
+++ contracts/ExercisePLYS.sol
@@ -6,9 +6,10 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "./interfaces/IElysian.sol";
 
 interface IVault {
-    function depositReseves(uint value, address to) external returns (bool);
+    function depositReserves(uint value, address to) external returns (bool);
 }
 
 interface IPLYS {
@@ -38,6 +39,11 @@ contract ExercisePLYS is Ownable, ReentrancyGuard {
         treasury = treasury_;
     }
 
+    function setTreasury(address treasury_) external onlyOwner returns (bool) {
+        treasury = treasury_;
+        return true;
+    }
+
     function setTerms(address vester, uint256 amountCanClaim, uint256 rate) external onlyOwner returns (bool) {
         require(amountCanClaim >= maxAllowedToClaim[vester], "Cannot lower amount claimable");
         require(rate >= percentCanVest[vester], "Cannot lower vesting rate");
@@ -47,27 +53,28 @@ contract ExercisePLYS is Ownable, ReentrancyGuard {
     }
 
     function exercisePLYS(uint256 amountToExercise) external nonReentrant returns (bool) {
-        require(getPLYSAbleToClaim(_msgSender()) >= amountToExercise, 'Not enough LYS vested');
+        //require(getPLYSAbleToClaim(_msgSender()) >= amountToExercise, 'Not enough LYS vested');
         require(maxAllowedToClaim[_msgSender()] >= amountClaimed[_msgSender()].add(amountToExercise), 'Claimed over max');
         
         IERC20(DAI).transferFrom(_msgSender(), address(this), amountToExercise);
         IERC20(DAI).approve(treasury, amountToExercise);
         
-        IVault(treasury).depositReseves( amountToExercise, _msgSender());
+        IVault(treasury).depositReserves( amountToExercise, _msgSender());
         require(IERC20(pLYS).balanceOf(_msgSender()) >= amountToExercise, "Not enough balance");
 
         IPLYS(pLYS).burnFrom(_msgSender(), amountToExercise);
         amountClaimed[_msgSender()] += amountToExercise;
-
+        
         return true;
     }
 
     function getPLYSAbleToClaim(address vester) public view returns (uint256) {
-        uint totalSupply = IERC20(LYS).totalSupply();
+        uint totalSupply = IElysian(LYS).totalSupply();
         return (totalSupply.mul(percentCanVest[vester].mul(1e9).div(10000))).sub(amountClaimed[vester]) ;
     }
 
-    function getTotalSupply() public view returns(uint) {
-        return IERC20(LYS).totalSupply();
+    function getTotalSupply() public view returns (uint) {
+        uint totalSupply = IElysian(LYS).totalSupply();
+        return totalSupply;
     }
 }
